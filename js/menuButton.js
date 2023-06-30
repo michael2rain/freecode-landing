f892167eb0022096d34c49f7bd4fdb30e279e4e8
Michael Barrera <soporte@michaelbarrera.dev>
2023-06-30 03:20:04 -0600

    feat: Added a toggle menu in mobile devices

---
+++ js/menuButton.js
@@ -0,0 +1,22 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    const hammy = document.getElementById("menuButton");
+    let firstClick = true;
+    
+    hammy.onclick = function() {
+        if (firstClick) {
+            // Primer click
+            const navLinks = document.getElementById("navLinks");
+            navLinks.classList.add("active");
+            hammy.classList.toggle('opened');
+            firstClick = false;
+        } else {
+            // Segundo click
+            hammy.classList.remove("opened");
+            const navLinks = document.getElementById("navLinks");
+            navLinks.classList.remove("active");
+            firstClick = true;
+        }
+    }
+    
+});
